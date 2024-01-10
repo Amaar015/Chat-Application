77e01dd535e374db7cc41e5a8a32baa2787d1230
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2024-01-10 10:57:06 +0500

    updated

--- src/sections/auth/LoginForm.jsx
+++ src/sections/auth/LoginForm.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import FormProvider from '../../components/hook-form/FormProvider';
 import * as Yup from 'yup';
-import { Password } from 'phosphor-react';
+import {useForm} from 'react-hook-form'
+import {yupResolver} from '@hookform/resolvers/yup';
+import { RHFTextField } from '../../components/hook-form';
+import { Alert, Stack } from '@mui/material';
 
 const LoginForm = () => {
   const [showpassword,Setshowpassword]=useState(false);
    const LoginSchema=Yup.object().shape({
-         email:Yup.string().required("Email is required").email("Email must be a valid email address");
+         email:Yup.string().required("Email is required").email("Email must be a valid email address"),
          Password: Yup.string().required("Password is required"),
    })
 
@@ -14,8 +17,39 @@ const LoginForm = () => {
           email:"razaammar582@gmail.com",
           password:"2715"
    }
+
+   const methods=useForm({
+     resolver:yupResolver(LoginForm),
+     defaultValues,
+   })
+
+   const {
+    reset,
+    setError,
+    handleSubmit,
+    formState:{errors,isSubmitting,isSubmitSuccessful},
+   }=methods;
+
+    const onSubmit=async(data)=>{
+      try {
+          // data handled from backed
+      } catch (error) {
+           console.log(error);
+           reset();
+           setError('after Submit',{
+            ...error,
+            message:error.message,
+           })
+      }
+    }
+
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+
+       <Stack spacing={3}>
+         {!!errors.afterSubmit && <Alert severity='error'>{errors.afterSubmit.message}</Alert>}
+        </Stack>    
+           <RHFTextField name='email' label="Email address"/>
 
     </FormProvider>
     
