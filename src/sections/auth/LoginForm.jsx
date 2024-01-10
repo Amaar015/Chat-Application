3ed967cd6829a2d07fc49985c42aa9438bd1cd6c
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2024-01-10 10:15:19 +0500

    updated

--- src/sections/auth/LoginForm.jsx
+++ src/sections/auth/LoginForm.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FormProvider from '../../components/hook-form/FormProvider';
+import * as Yup from 'yup';
+import { Password } from 'phosphor-react';
 
 const LoginForm = () => {
+  const [showpassword,Setshowpassword]=useState(false);
+   const LoginSchema=Yup.object().shape({
+         email:Yup.string().required("Email is required").email("Email must be a valid email address");
+         Password: Yup.string().required("Password is required"),
+   })
+
+   const defaultValues={
+          email:"razaammar582@gmail.com",
+          password:"2715"
+   }
   return (
-    <div>
-       hello sir bare log
-    </div>
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+
+    </FormProvider>
+    
   )
 }
 
