6cc5dc2e4b7e144d6aa663072cf2c237dea8d639
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2024-01-11 18:04:53 +0500

    updated

--- src/components/hook-form/index.jsx
+++ src/components/hook-form/index.jsx
@@ -1,2 +1,4 @@
-export {default} from './FormProvider'
-export {default as RHFTextField} from './RHFTextField' 
+export { default } from './FormProvider';
+
+
+export { default as RHFTextField } from './RHFTextField';
--- src/sections/auth/LoginForm.jsx
+++ src/sections/auth/LoginForm.jsx
@@ -1,91 +1,117 @@
-import React, { useState } from 'react'
-import FormProvider from '../../components/hook-form/FormProvider';
-import * as Yup from 'yup';
-import {useForm} from 'react-hook-form'
-import {yupResolver} from '@hookform/resolvers/yup';
-import { RHFTextField } from '../../components/hook-form';
-import { Alert, Button, IconButton, InputAdornment, Link, Stack } from '@mui/material';
-import { Eye, EyeSlash } from 'phosphor-react';
+import { useState } from "react";
+import * as Yup from "yup";
+// form
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+// @mui
+import { Link, Stack, Alert, IconButton, InputAdornment, Button } from "@mui/material";
+// import { LoadingButton } from "@mui/lab";
+// components
+import FormProvider, { RHFTextField } from "../../components/hook-form";
+import { Eye, EyeSlash } from "phosphor-react";
+// import { LoginUser } from "../../redux/slices/auth";
+// import { useDispatch, useSelector } from "react-redux";
 
-const LoginForm = () => {
-  const [showpassword,Setshowpassword]=useState(false);
-   const LoginSchema=Yup.object().shape({
-         email:Yup.string().required("Email is required").email("Email must be a valid email address"),
-         Password: Yup.string().required("Password is required"),
-   })
+// ----------------------------------------------------------------------
 
-   const defaultValues={
-          email:"razaammar582@gmail.com",
-          password:"2715"
-   }
+export default function AuthLoginForm() {
+  // const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
 
-   const methods=useForm({
-     resolver:yupResolver(LoginForm),
-     defaultValues,
-   })
+  // const {isLoading} = useSelector((state) => state.auth);
 
-   const {
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const defaultValues = {
+    email: "demo@tawk.com",
+    password: "demo1234",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
     reset,
     setError,
     handleSubmit,
-    formState:{errors,isSubmitting,isSubmitSuccessful},
-   }=methods;
+    formState: { errors },
+  } = methods;
 
-    const onSubmit=async(data)=>{
-      try {
-          // data handled from backed
-      } catch (error) {
-           console.log(error);
-           reset();
-           setError('after Submit',{
-            ...error,
-            message:error.message,
-           })
-      }
+  const onSubmit = async (data) => {
+    try {
+      // console.log(data);
+      // submit data to backend
+      // dispatch(LoginUser(data));
+    } catch (error) {
+      console.error(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
     }
+  };
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
 
-       <Stack spacing={3}>
-         {!!errors.afterSubmit && <Alert severity='error'>{errors.afterSubmit.message}</Alert>}
-            
-           <RHFTextField name='email' label="Email address"/>
-              
-           <RHFTextField name='password' label='Password' type={showpassword ? "text" : "password"} InputProps={{
-            endAdornment:(
-                  <InputAdornment>
-                    <IconButton onClick={()=>{Setshowpassword(!showpassword)}}>
-                      {showpassword ? <Eye/> : <EyeSlash/>}
-                    </IconButton>
-                  </InputAdornment>
-            )
-           }}/>   
+        <RHFTextField name="email" label="Email address" />
+
+        <RHFTextField
+          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
       </Stack>
-       <Stack alignItems={'flex-end'} sx={{my:2}}>
-             <Link variant='body2' color='inherit' underline='always'>
-              Forgot Password?
-             </Link>
-       </Stack>
-        <Button
-        fullWidth
-        color='inherit'
-        size='large'
-        type='submit'
-        variant='contained'
-        sx={{
-          bgcolor:"text.primary",
-          color:(theme)=>
-          theme.palette.mode === "light" ? "common.white" : "grey.800",
-          "&:hover":{
-            bgcolor:"text.primary",
-            color:(theme)=>
-            theme.palette.mode==='light' ? "common.white": "grey.800",
-          }
-        }}>Login</Button>
-    </FormProvider>
-    
-  )
-}
 
-export default LoginForm
+      <Stack alignItems="flex-end" sx={{ my: 2 }}>
+        <Link   variant="body2" color="inherit" underline="always">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <Button
+        fullWidth
+        color="inherit"
+        size="large"
+        type="submit"
+        variant="contained"
+        sx={{
+          bgcolor: "text.primary",
+          color: (theme) =>
+            theme.palette.mode === "light" ? "common.white" : "grey.800",
+          "&:hover": {
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+          },
+        }}
+      >
+        Login
+      </Button>
+    </FormProvider>
+  );
+}
